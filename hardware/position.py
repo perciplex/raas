ee0500e2a8287ce62c15615825aa0cb2e66acc1b
philzook58 <philzook58@gmail.com>
2019-09-01 18:28:01 -0400

    poition

---
+++ hardware/position.py
@@ -0,0 +1,9 @@
+from driver import Encoder, Motor
+import numpy as np
+motor = Motor()
+encoder = Encoder()
+
+while True:
+    angle = encoder.getRadian()
+    torque = 1000 * np.sin(angle) 
+    motor.set_torque(torque)
