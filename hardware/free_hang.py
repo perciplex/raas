fa72a8b5e7d2246bdd348aad8867bd1ac954d789
philzook58 <philzook58@gmail.com>
2019-09-08 12:01:10 -0400

    simple calibration script

--- hardware/free_hang.py
+++ hardware/free_hang.py
@@ -1,17 +1,17 @@
 from driver import Encoder, Motor
 from time import sleep, time
 import pickle 
-e = Encoder()
+enc = Encoder()
 
-time = 1.0
+T = 1.0
 dt = 0.01
-steps = time / dt
+steps = T / dt
 
 angles = []
 start_time = time()
 for i in range(steps):
     sleep(dt)
-    angles.append(e.getRadian())
+    angles.append(enc.getRadian())
 total_time = time() - start_time
 print(total_time)
 pickle.dump(angles, open( "free_run.p", "wb"))
