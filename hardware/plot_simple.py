485cec3538b4ba334b9d0e7d522b28c301dd7c46
philzook58 <philzook58@gmail.com>
2019-09-08 12:43:06 -0400

    new routine

---
+++ hardware/plot_simple.py
@@ -0,0 +1,22 @@
+import pickle
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import numpy as np
+angles = pickle.load( open( "simple_run.p", "rb" ) )
+
+t = np.linspace(0,1,len(angles))
+'''
+def decay_sin(t, omega, dtheta, offset, decay, amp):
+    return amp * np.sin(omega * t + dtheta) * np.exp(- decay * t)  +  offset
+opt, _ = curve_fit(decay_sin, t,angles)
+
+print(opt)
+'''
+plt.plot(t,angles)
+plt.title("command of 500")
+plt.xlabel("time (s)")
+plt.ylabel("radians")
+#plt.plot(t, decay_sin(t, *opt) )
+
+
+plt.show()
--- hardware/simple_run.py
+++ hardware/simple_run.py
@@ -9,18 +9,24 @@ T = 1.0
 dt = 0.01
 steps = int(T / dt)
 
-angles = []
 
-start_time = time()
-m.set_torque(500)
-for i in range(steps):
-    sleep(dt)
-    angles.append(e.getRadian())
-m.stop()
-total_time = time() - start_time
-print(total_time)
+anglemap = {}
 
-pickle.dump(angles, open( "simple_run.p", "wb"))
+for torque in range(200,1001,200):
+    angles = []
+    start_time = time()
+    m.set_torque(torque)
+    for i in range(steps):
+        sleep(dt)
+        angles.append(e.getRadian())
+    m.stop()
+    total_time = time() - start_time
+    print(total_time)
+    anglemap[torque] = angles
+    sleep(1.5)
+    
+
+pickle.dump(anglemap, open( "simple_run.p", "wb"))
 
 
 
