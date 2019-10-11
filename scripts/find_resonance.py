e35e3b8c981a00bdcfa4d36e6fee06fc9182dcbd
declanoller <declanoller@gmail.com>
2019-10-10 23:24:36 -0400

    fixed stupid error

--- scripts/find_resonance.py
+++ scripts/find_resonance.py
@@ -33,7 +33,7 @@ for w in w_range:
         if t % max(N_steps/10, 1) == 0:
             print(f'Step {t}')
             print('\tAction: {}'.format(action))
-            print('\tState: {}, reward: {}'.format(s_next, r))
+            print('\tState: {}, reward: {:.3f}'.format(s_next, r))
 
         x, y, _ = s_next
 
@@ -43,10 +43,11 @@ for w in w_range:
         s = s_next
 
     max_amps.append(max_ep_amp)
+    print(f'\nMax episode amplitude = {max_ep_amp:.2f}')
 
-    print('Resetting...')
+    print('\nResetting...')
     s_next, r, done, _ = env_pend.step(np.array([0.0]))
-    sleep(3.0)
+    sleep(5.0)
 
 
 print(w_range.tolist())
