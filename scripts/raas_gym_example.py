65adcbd9f521530bc79283366bda56462a217253
declanoller <declanoller@gmail.com>
2019-10-10 22:32:53 -0400

    fixed stupid error

--- scripts/raas_gym_example.py
+++ scripts/raas_gym_example.py
@@ -9,9 +9,16 @@ print('Creating gym object...')
 env_pend = raas_gym.make('Pendulum-v0')
 
 N_steps = 20
+s = None
 for i in range(N_steps):
     print(f'Step {i}')
-    s_next, r, done, _ = env_pend.step(np.array([0.0]))
+    action = np.array([0.0])
+    print('\tAction: {}'.format(action))
+    s_next, r, done, _ = env_pend.step(action)
+    print('\tState: {}, reward: {}'.format(s_next, r))
+
+
+    s = s_next
 
 
 
