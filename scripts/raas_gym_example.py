93454ae76c99bd8636da1a528318b976e47a148f
declanoller <declanoller@gmail.com>
2019-10-10 22:47:13 -0400

    fixed stupid error

--- scripts/raas_gym_example.py
+++ scripts/raas_gym_example.py
@@ -8,11 +8,11 @@ print('Creating gym object...')
 # Creates and returns Pendulum env object
 env_pend = raas_gym.make('Pendulum-v0')
 
-N_steps = 200
+N_steps = 300
 s = None
 for t in range(N_steps):
     print(f'Step {t}')
-    action = np.array([np.sin(0.1*t)])
+    action = np.array([2.0*np.sin(0.05*t)])
     print('\tAction: {}'.format(action))
     s_next, r, done, _ = env_pend.step(action)
     print('\tState: {}, reward: {}'.format(s_next, r))
