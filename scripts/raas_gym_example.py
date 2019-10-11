edec17568492a8877d19754b93c34d2f27b51a4c
declanoller <declanoller@gmail.com>
2019-10-10 22:50:52 -0400

    fixed stupid error

--- scripts/raas_gym_example.py
+++ scripts/raas_gym_example.py
@@ -1,6 +1,10 @@
 import path_utils
 import raas_gym
 import numpy as np
+from time import sleep
+
+
+# natural freq: omega = 5.2 ==> f = 5.2/5.3 = 0.82
 
 
 
@@ -9,10 +13,15 @@ print('Creating gym object...')
 env_pend = raas_gym.make('Pendulum-v0')
 
 N_steps = 300
+time_incr = 0.05
+w = 5.2
 s = None
 for t in range(N_steps):
+
+    sleep(time_incr)
+
     print(f'Step {t}')
-    action = np.array([2.0*np.sin(0.05*t)])
+    action = np.array([2.0*np.sin(w*t*time_incr)])
     print('\tAction: {}'.format(action))
     s_next, r, done, _ = env_pend.step(action)
     print('\tState: {}, reward: {}'.format(s_next, r))
