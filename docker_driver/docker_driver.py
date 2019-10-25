4576183387f5504de9dd134bc5be147b84e342ab
Ben <ishmandoo@gmail.com>
2019-10-24 21:41:44 -0400

    updated docker_driver with the new server interface

--- docker_driver/docker_driver.py
+++ docker_driver/docker_driver.py
@@ -2,13 +2,16 @@ import docker
 import requests
 import argparse
 import time
+from pathlib import *
 
 
 def launch_docker(gitUrl="https://github.com/perciplex/raas-starter.git"):
     client = docker.from_env()
-    dockerfile = "docker/."
+    
+    dockerfile = str(Path.cwd() / 'docker')
     docker_tag = "raas-dev-test:latest"
 
+    print(dockerfile)
     # build the final image using the local raas-base, eventually need to pass in git url
     response = client.images.build(
         path=dockerfile, tag=docker_tag, buildargs={"GIT_REPO_URL": gitUrl}
@@ -46,15 +49,17 @@ server_ip = args.server
 while True:
 
     response = requests.get(server_ip + "/job/pop")
-    print(server_ip + "/job/pop")
+    print(response)
 
     # If work is found, launch the work
     if response.status_code == 200:
         # Get response json
         job_json = response.json()
+        print(job_json)
         job_id = job_json["id"]
-        gitUrl = job_json["gitUrl"]
-        results = launch_docker(gitUrl)
+        gitUrl = job_json["git_url"]
+        #results = launch_docker(gitUrl)
+        results = launch_docker()
         job_json["results"] = results
         requests.put("/job/%d/results" % job_id, json=job_json)  # json or data?
     else:
