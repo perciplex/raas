415d76b0de770b06eb0efc4d88cbe9eb0af6f8e6
MaximilianKing <Maximilian_King@brown.edu>
2019-12-19 23:41:37 -0500

    Fixed services. Black

--- docker_driver/led_driver.py
+++ docker_driver/led_driver.py
@@ -4,37 +4,59 @@ import time
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.legacy import text, show_message
-from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+from luma.core.legacy.font import (
+    proportional,
+    CP437_FONT,
+    TINY_FONT,
+    SINCLAIR_FONT,
+    LCD_FONT,
+)
 import multiprocessing
 
 # a class for displaying a single message on the LED matrix display
 
-class LedMessage():
-        def __init__(self, msg):
-                self.msg = msg # store the message content
-                self.serial = spi(port=0, device=0, gpio=noop()) # open a serial port
-                self.device = max7219(self.serial, cascaded=4, block_orientation=90,rotate=2, blocks_arranged_in_reverse_order=True) # initialize a matrix display object
-                self.device.contrast(6) # decrease the contrast so that it looks okay on video
-                self.process = None # initialize a variable that will store the process 
 
-        def __del__(self):
-                self.stop() # stop any message and clear
-                self.serial.cleanup() # clean up the serial port
+class LedMessage:
+    def __init__(self, msg):
+        self.msg = msg  # store the message content
+        self.serial = spi(port=0, device=0, gpio=noop())  # open a serial port
+        self.device = max7219(
+            self.serial,
+            cascaded=4,
+            block_orientation=90,
+            rotate=2,
+            blocks_arranged_in_reverse_order=True,
+        )  # initialize a matrix display object
+        self.device.contrast(6)  # decrease the contrast so that it looks okay on video
+        self.process = None  # initialize a variable that will store the process
 
+    def __del__(self):
+        self.stop()  # stop any message and clear
+        self.serial.cleanup()  # clean up the serial port
 
-        def start(self):
-                self.process = multiprocessing.Process(target=self._show_msg, args=(), kwargs={}) # initialize a process to repeatedly write the message
-                self.process.start() # start the process
+    def start(self):
+        self.process = multiprocessing.Process(
+            target=self._show_msg, args=(), kwargs={}
+        )  # initialize a process to repeatedly write the message
+        self.process.start()  # start the process
 
-        def stop(self):
-                if self.process: # check if a process is actually running
-                        self.process.terminate() # if it has, stop it
-                        self.process = None # reset the process to none
-                self._clear() # clear the display
+    def stop(self):
+        if self.process:  # check if a process is actually running
+            self.process.terminate()  # if it has, stop it
+            self.process = None  # reset the process to none
+        self._clear()  # clear the display
 
-        def _show_msg(self):  
-                while True: # forever
-                        show_message(self.device, self.msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.05) # display the scrolling message
+    def _show_msg(self):
+        while True:  # forever
+            show_message(
+                self.device,
+                self.msg,
+                fill="white",
+                font=proportional(LCD_FONT),
+                scroll_delay=0.05,
+            )  # display the scrolling message
 
-        def _clear(self):
-                show_message(self.device, "", fill="white", font=proportional(LCD_FONT), scroll_delay=0) # write a blank message to clear the screen
+    def _clear(self):
+        show_message(
+            self.device, "", fill="white", font=proportional(LCD_FONT), scroll_delay=0
+        )  # write a blank message to clear the screen
--- services/README.md
+++ services/README.md
@@ -1,14 +1,14 @@
 Explicitly, these things need to get done in order for the service to begin. Will make a nice lil script for it
 
-`sudo cp services/container_scheduler.service /lib/systemd/system/`
-`sudo cp services/motor_controller.service /lib/systemd/system/`
+`sudo cp services/raas_cs.service /lib/systemd/system/`
+`sudo cp services/raas_mc.service /lib/systemd/system/`
 
-`sudo chmod 644 /lib/systemd/system/container_scheduler.service`
-`sudo chmod 644 /lib/systemd/system/motor_controller.service`
+`sudo chmod 644 /lib/systemd/system/raas_cs.service`
+`sudo chmod 644 /lib/systemd/system/raas_mc.service`
 
 `sudo systemctl daemon-reload`
-`sudo systemctl enable container_scheduler.service`
-`sudo systemctl enable motor_controller.service`
+`sudo systemctl enable raas_cs.service`
+`sudo systemctl enable raas_mc.service`
 
-`sudo systemctl start container_scheduler.service`
-`sudo systemctl start container_scheduler.service`
+`sudo systemctl start raas_mc.service`
+`sudo systemctl start raas_cs.service`
