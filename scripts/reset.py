7400423bb522aeeb553487d6961c8d51d4760311
declanoller <declanoller@gmail.com>
2020-01-09 21:08:07 -0500

    reset script

---
+++ scripts/reset.py
@@ -0,0 +1,19 @@
+import gym
+import gym_raas
+
+from time import sleep
+import numpy as np
+
+settle_time = 30.0
+
+# create env
+env = gym.make('raaspendulum-v0')
+env.reset()
+
+sleep(1.0)
+print('Giving kick to make sure pendulum still isnt standing up...')
+_, _, _, _ = env.step([2]) # start with an initial impulse
+print('Done, sleeping for {} seconds to let it settle...'.format(settle_time))
+sleep(settle_time)
+
+print('Done!')
