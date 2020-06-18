556c9d3f9b388967480f2e189fb7376bef07a3ac
Ben <ishmandoo@gmail.com>
2020-06-18 02:25:16 +0100

    docker test

---
+++ docker_driver/test_docker.py
@@ -0,0 +1,92 @@
+import docker
+import requests
+import argparse
+import time
+from pathlib import Path
+import json
+import socket
+import os
+
+from led_driver import LedMessage
+
+import reset_pendulum
+
+def launch_docker(gitUrl="https://github.com/perciplex/raas-example.git"):
+    client = docker.from_env()
+
+    dockerfile = str(Path(__file__).resolve().parent / "docker_images/final_image")
+    docker_tag = "raas-dev-test:latest"
+
+    print(dockerfile)
+    print(docker_tag)
+    print(gitUrl)
+
+    with open("/tmp/log.json", "w") as f:
+        pass
+
+    os.chmod("/tmp/log.json", 0o777)
+
+    # build the final image using the local raas-base, eventually need to pass in git url
+    response = client.images.build(
+        path=dockerfile,
+        tag=docker_tag,
+        buildargs={"GIT_REPO_URL": gitUrl},
+        nocache=True,
+    )
+
+    # try to make the internal network which disables external network traffic, fails gracefully
+    try:
+        client.networks.create(
+            "docker_internal", driver="bridge", internal=True, check_duplicate=True
+        )
+    except docker.errors.APIError:
+        pass
+
+    failed = False
+    data = None
+
+    try:
+        stdout = client.containers.run(
+            docker_tag,
+            mounts=[{"Type": "bind", "Source": "/tmp/", "Target": "/tmp/", "RW": True}],
+            network="docker_internal",
+            # auto_remove=True,
+            mem_limit="3g",
+        )
+
+    except docker.errors.ContainerError as e:
+        print(dir(e.container))
+        stdout = e.container.logs()
+        failed = True
+        print(stdout)
+    except Exception as e:
+        stdout = e
+        failed = True
+        print(stdout)
+
+    try:
+        with open("/tmp/log.json") as f:
+            data = json.load(f)
+
+    except Exception as e:
+        print(f"Error {e}")
+
+    return str(stdout), data, failed
+
+    """ok. When this program stops it kills the container.
+    Ideally I'd like to create the ccontsiner and transfer
+    file into it, then start it. But I can't figure out how?
+    We could also build a custom dockerfile that is made from our base image.
+    build and run that."""
+
+user = "foo"
+name = "bar"
+led = LedMessage(f"{user}:{name}")
+led.start()
+res = launch_docker()
+reset_pendulum.reset_pendulum()
+
+led.stop()
+
+print(res)
+time.sleep(1)
