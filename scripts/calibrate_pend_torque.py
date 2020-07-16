0373fdb0482add3e1a0d53068b787ad9674e6782
declanoller <declanoller@gmail.com>
2020-07-15 21:03:47 -0400

    added calibration script

---
+++ scripts/calibrate_pend_torque.py
@@ -0,0 +1,26 @@
+import gym
+import gym_raas
+import numpy as np
+import time
+
+env = gym.make("raaspendulum-v0")
+env.reset()
+obs = []
+
+torque_range = np.linspace(0, 2.0, 10)
+
+
+try:
+    for t in torque_range:
+
+        observation, reward, done, info = env.step([torque])
+        time.sleep(5.0)
+        obs.append(observation)
+
+except:
+    print("Stopped!")
+
+
+[print(o) for o in obs]
+
+env.reset()
