c97ad91b52a15f53ced624689c21fd6431002ea9
Maximilian King <mxln.king@gmail.com>
2020-07-29 23:36:03 -0400

    CS Cleanup and DB conn class overhaul (#50)
    * Refactor of db fns, new class for conn.
    
    * DB conn class finished and working
    
    * Updated fns names, added reconnect functionality
    
    * Added requirements, common values for server, job_db_dao updates, working front end
    
    * Updated job dao, removed unused stuff in application, working flask and react local
    
    * Updates to application, cs
    
    * Fixed ravioloi method
    
    * Updated application to sort completed and failed by end time

--- server/application.py
+++ server/application.py
@@ -1,15 +1,16 @@
-import queue
 import random
 import sys
 import time
 import uuid
+import logging as log
 from collections import deque
 from json import JSONEncoder
+from common import JobStatus
+from hardware import Hardware
 
-import database_fns
+from job_db_dao import JobDbDao
 from flask import Flask, jsonify, make_response, redirect, request
 
-from hardware import Hardware
 
 rd = random.Random()
 rd.seed(0)
@@ -19,22 +20,17 @@ application.config.from_pyfile("config.cfg")
 # sslify = SSLify(application)
 
 
-# a status enum
-class Status:
-    QUEUED = "QUEUED"
-    RUNNING = "RUNNING"
-    COMPLETE = "COMPLETE"
-    FAILED = "FAILED"
-
-
-# a class for queued, running, or completed jobs
 class Job:
+    """
+    Class for queued, running, or completed jobs
+    """
+
     def __init__(self, git_user, project_name, git_url):
         self.id = str(uuid.UUID(int=rd.getrandbits(128)))  # a random id
         self.project_name = project_name  # github project name
         self.git_user = git_user  # github user id
         self.git_url = git_url  # github hrl
-        self.status = Status.QUEUED  # job status
+        self.status = JobStatus.QUEUED  # job status
         self.hardware_name = None  # the hardware the job is/was run on, none if queued
         self.stdout = "Results pending."  # job results
         # observations, actions, reqards, and times for the job data points
@@ -70,11 +66,17 @@ class Job:
         }
 
 
-# A class that holds the most recent cached results, so the frontend won't
-# ping the database for each page load.
 class JobsCache:
-    def __init__(self):
+    """
+    A class that holds the most recent cached results, so the frontend won't
+    ping the database for each page load.
 
+    args:
+       job_dao (JobDbDao):  Job DB Dao class object for jobs DB
+    """
+
+    def __init__(self, job_dao):
+        self.job_db_dao = job_dao
         self.last_db_read_time = time.time()
         self.update_period_seconds = 1.0  # In seconds
         self.cache = {
@@ -106,15 +108,20 @@ class JobsCache:
                 return {}
 
         self.last_db_read_time = time.time()
-        q = list_to_dict(database_fns.get_all_jobs_by_status("QUEUED", "ASC"))
-        r = list_to_dict(database_fns.get_all_jobs_by_status("RUNNING", "ASC"))
-        c = list_to_dict(database_fns.get_all_jobs_by_status("COMPLETED", "DESC"))
+        q = list_to_dict(self.job_db_dao.get_jobs_by_status(JobStatus.QUEUED))
+        r = list_to_dict(self.job_db_dao.get_jobs_by_status(JobStatus.RUNNING))
+        c = list_to_dict(
+            self.job_db_dao.get_jobs_by_status(JobStatus.COMPLETED, sort_order="DESC")
+        )
+        f = list_to_dict(
+            self.job_db_dao.get_jobs_by_status(JobStatus.FAILED, sort_order="DESC")
+        )
 
         self.cache = {
             "queued": deque(q.values()),
             "running": r,
-            "completed": c,
-            "all_jobs": {**q, **r, **c},
+            "completed": {**c, **f},
+            "all_jobs": {**q, **r, **c, **f},
         }
 
     def get_job_in_cache_from_id(self, id, cache="all_jobs"):
@@ -128,12 +135,12 @@ class JobsCache:
         Returns:
             job (dict)
         """
-        if self.check_job_id_in_cache(id, cache=cache):
+        if self.check_job_in_cache_from_id(id, cache=cache):
             return self.cache[cache][id]
         else:
             return None
 
-    def check_job_id_in_cache(self, id, cache="all_jobs"):
+    def check_job_in_cache_from_id(self, id, cache="all_jobs"):
         """
         Checks if the job id exists in the cache.
 
@@ -147,13 +154,15 @@ class JobsCache:
         return id in self.cache[cache]
 
 
-# Custom json encoder which replaces the default and allows Job objects to be jsonified
 class JSONEncoderJob(JSONEncoder):
+    """
+    Custom json encoder which jsonifies Job objects. Inherits JSONEncoder
+    """
+
     def default(self, job):
         try:
-            if isinstance(
-                job, Job
-            ):  # if the object to be encoded is a job, use the dict() function
+            # if the object to be encoded is a job, use the dict() function
+            if isinstance(job, Job):
                 return job.__dict__()
 
         except TypeError:
@@ -161,28 +170,19 @@ class JSONEncoderJob(JSONEncoder):
         return JSONEncoder.default(self, job)
 
 
-jobs = {}
-queued = queue.Queue()  # a queue for the queued jobs
-running = {}  # a set of running jobs
-completed = queue.LifoQueue(maxsize=20)  # a queue of recently completed jobs
-
-hardware_list = ["Omar", "Goose", "Nicki", "Beth"]
-hardware_dict = {name: Hardware(name) for name in hardware_list}
-jobs_cache = JobsCache()
-
-
-def reset_jobs():
-    global jobs, queued, running, completed
-    jobs = {}
-
-    queued = queue.Queue()  # a queue for the queued jobs
-    running = {}  # a set of running jobs
-    completed = queue.Queue(maxsize=20)  # a queue of recently completed jobs
-
-
 def check_password(password):
-    if password == application.config["FLASK_PASS"]:
-        print("Bad password from from host: {}".format(request.args.get("hardware")))
+    """
+    Checks that the request has required password.
+
+    args:
+        password (str): Password supplied by request.
+    returns:
+        bool:           If password matches flask config
+    """
+    if password != application.config["FLASK_PASS"]:
+        log.error(
+            "Bad password from from host: {}".format(request.args.get("hardware"))
+        )
         return False
     else:
         return True
@@ -192,17 +192,21 @@ def check_password(password):
 def index():
     return application.send_static_file("index.html")
 
-@application.route('/favicon.ico')
+
+@application.route("/favicon.ico")
 def favicon():
     return application.send_static_file("favicon.ico")
 
-@application.route('/favicon-16x16.png')
+
+@application.route("/favicon-16x16.png")
 def favicon16():
     return application.send_static_file("favicon-16x16.png")
 
-@application.route('/favicon-32x32.png')
+
+@application.route("/favicon-32x32.png")
 def favicon32():
     return application.send_static_file("favicon-32x32.png")
+
 
 @application.route("/api/hardware")
 def hardware_route():
@@ -234,19 +238,21 @@ def job_page_route(id):
 def job_route():
     jobs_cache.get_db_cache()
     if request.method == "POST":
-        print(request)
+        log.info("New job: {}".format(request))
         git_user, project_name, git_url = (
             request.form["git_user"],
             request.form["project_name"],
             request.form["git_url"],
         )
 
+        # If git project already in queued cache, dont create job.
         for job in jobs_cache.cache["queued"]:
             if (git_user, project_name) == (job["git_user"], job["project_name"],):
+                log.info("Job already in queued. Skipping creation.")
                 return redirect("/")
 
         # Else, add new job
-        database_fns.new_job(project_name, git_url, git_user)
+        job_db_dao.insert_new_job(project_name, git_url, git_user)
         return redirect("/")
 
     # Need to update with DB stuff
@@ -255,7 +261,11 @@ def job_route():
             {
                 "queued": (list(jobs_cache.cache["queued"])),
                 "running": (list(jobs_cache.cache["running"].values())),
-                "completed": (list(jobs_cache.cache["completed"].values())),
+                "completed": sorted(
+                    list(jobs_cache.cache["completed"].values()),
+                    key=lambda i: i["end_time"],
+                    reverse=True,
+                ),
             }
         )
 
@@ -268,17 +278,20 @@ def job_pop_route():
             return make_response("Bad password.", 403)
 
         req_hardware = request.args.get("hardware")
-        if req_hardware in hardware_dict:
-            hardware_dict[req_hardware].heartbeat()
+        if req_hardware not in hardware_dict:
+            log.error("Hardware {} not found in hardware_dict".format(req_hardware))
+            return make_response("Hardware not found in hardware_dict", 500)
 
+        hardware_dict[req_hardware].heartbeat()
+
+        # If there are queued jobs, pop one, update the db row, and return the job
         if jobs_cache.cache["queued"]:
+            hardware_dict[req_hardware].starting_job()
 
             pop_job = jobs_cache.cache["queued"].pop()  # get job from queue
-            database_fns.start_job(pop_job["id"], req_hardware)
+            job_db_dao.update_start_job(pop_job["id"], req_hardware)
 
-            if req_hardware in hardware_dict:
-                hardware_dict[req_hardware].starting_job()
-            print("{} has popped job {}.".format(req_hardware, pop_job["id"]))
+            log.info("{} has popped job {}.".format(req_hardware, pop_job["id"]))
             return jsonify(pop_job)
         else:
             return make_response("", 204)
@@ -286,20 +299,22 @@ def job_pop_route():
 
 @application.route("/api/job/<string:id>/results", methods=["PUT"])
 def job_results_route(id):
-    jobs_cache.get_db_cache()
     req_hardware = request.args.get("hardware")
     if request.method == "PUT":
+        status = request.json["failed"]
         if not check_password(request.args["FLASK_PASS"]):
             return make_response("", 403)
-        if jobs_cache.check_job_id_in_cache(id):
 
-            database_fns.end_job(id, not request.args.get("hardware"))
-            print("{} has completed job {}.".format(req_hardware, id))
-            return make_response("", 200)
-        else:
-            print("{} has failed to complete job {} in cache.".format(req_hardware, id))
-            return make_response("", 404)
+        job_db_dao.update_end_job(id, status)
+        log.info("{} has completed job {}.".format(req_hardware, id))
+        return make_response("", 200)
 
+
+hardware_list = ["Omar", "Goose", "Nicki", "Beth"]
+hardware_dict = {name: Hardware(name) for name in hardware_list}
+
+job_db_dao = JobDbDao()
+jobs_cache = JobsCache(job_db_dao)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "prod":
