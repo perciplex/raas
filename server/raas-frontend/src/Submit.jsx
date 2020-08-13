a228cec32fe3e873a13c5f8bed2d0f51d41fd0e5
MaximilianKing <Maximilian_King@brown.edu>
2020-08-12 22:15:25 -0400

    Updated language on what i do page

--- server/raas-frontend/src/Submit.jsx
+++ server/raas-frontend/src/Submit.jsx
@@ -9,21 +9,25 @@ function Submit() {
             <Row>
                 <Col>
                     <Card>
-                        <Card.Header>what do I do?</Card.Header>
+                        <Card.Header>What do I do?</Card.Header>
                         <Card.Body>
-                            Start by forking <a href="https://github.com/perciplex/raas-starter">our starter repository on GitHub</a>, or build your own repository. Either way, you must include <code>run.py</code> at the root directory. We use the same interface as OpenAI Gym with a special enviorment.
+                            Start by forking <a href="https://github.com/perciplex/raas-starter">our starter repository on GitHub</a>, or build your own repository. Either way, you must include <code>run.py</code> at the root directory. We use the same interface as OpenAI Gym with a <a href="https://github.com/perciplex/gym-raas">custom environment</a>.
                                 <pre><code>
                                 {`
+                                 
 import gym
-<strong>import raas_envs</strong>
-env = gym.make('<strong>Pendulum-RaaS</strong>')
+import gym_raas
+
+env = gym.make('raaspendulum-v0')
 env.reset()
+
 for _ in range(1000):
     env.render()
     env.step(env.action_space.sample()) # take a random action
+
 env.close()`}
                             </code></pre>
-                            When run locally, the above code performs a Gym simulation with phyical parameters set to
+                            When run locally, the above code performs a Gym simulation with physical parameters set to
                             match our hardware. When run at RaaS, the code controls an actual robot!
                         </Card.Body>
                     </Card>
